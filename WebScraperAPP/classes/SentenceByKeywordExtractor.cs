d4194abba77d666dc2e5887f75629a92d6350979
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-02-21 17:43:40 +0200

    Added KeywordExtractorClass

---
+++ WebScraperAPP/classes/SentenceByKeywordExtractor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraperAPP.classes
+{
+    class Class1
+    {
+    }
+}
